1206caa503d1af6c7dff57f28d8e461e263b40fc
Vyacheslav Saley <vyacheslav.saley@intexsoft.by>
2022-05-12 09:09:36 +0300

    Second commit

--- src/main/java/by/salei/rental/controller/AuthAccountController.java
+++ src/main/java/by/salei/rental/controller/AuthAccountController.java
@@ -1,16 +1,23 @@
 package by.salei.rental.controller;
 
 import by.salei.rental.dto.AuthAccountReg;
+import by.salei.rental.entity.AuthAccount;
 import by.salei.rental.entity.OrderStatus;
+import by.salei.rental.entity.Profile;
 import by.salei.rental.exception.UserAlreadyExist;
 import by.salei.rental.repo.AuthAccountRepository;
 import by.salei.rental.repo.CarRepository;
 import by.salei.rental.repo.OrderRepository;
 import by.salei.rental.service.api.AuthAccountService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.beans.Encoder;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,6 +28,7 @@ public class AuthAccountController {
     private final OrderRepository orderRepository;
     private final AuthAccountRepository repository;
     private final CarRepository carRepository;
+    private final PasswordEncoder encoder;
 
     @PostMapping("/create")
     public ModelAndView createAuthAccount(AuthAccountReg aar) {
@@ -37,6 +45,31 @@ public class AuthAccountController {
         }
 
         mv.setViewName("redirect:/auth/login");
+        return mv;
+    }
+
+    @PostMapping("/update")
+    public ModelAndView updateUserAccount(AuthAccountReg account) {
+
+        String userName = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        AuthAccount aa = repository.findByLogin(userName);
+        ModelAndView mv = new ModelAndView("redirect:/authAccount/" + aa.getId());
+        Profile p = aa.getProfile();
+
+        if(account.getPassword() != null) {
+            aa.setPassword(encoder.encode(account.getPassword()));
+        }
+        p.setFirstName(account.getFirstName());
+        p.setLastName(account.getLastName());
+        p.setMiddleName(account.getMiddleName());
+        p.setPhoneNumber(account.getPhoneNumber());
+
+        aa.setLogin(account.getUsername());
+        aa.setEmail(account.getEmail());
+        aa.setProfile(p);
+        repository.save(aa);
+
         return mv;
     }
 
